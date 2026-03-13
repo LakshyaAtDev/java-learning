ab68b812bfba32b513a2fe3f419c73d67034f0aa
Lakshya137 <lakshyasinghkaira341@gmail.com>
2026-03-13 22:49:12 +0530

    java calculator
    For calculating percentage

---
+++ java calculator.java
@@ -0,0 +1,25 @@
+package cal.cal;
+import java.util.Scanner;
+public class percentage {
+    static void main() {
+        System.out.println("To calculate your percentage");
+        System.out.println("Enter marks in subject 1");
+        Scanner scanner=new Scanner(System.in);
+        float S1=scanner.nextFloat();
+        System.out.println("Enter marks in subject 2");
+        float S2=scanner.nextFloat();
+        System.out.println("Enter marks in subject 3");
+        float S3=scanner.nextFloat();
+        System.out.println("Enter marks in subject 4");
+        float S4=scanner.nextFloat();
+        System.out.println("Enter marks in subject 5");
+        float S5=scanner.nextFloat();
+        System.out.println("Now enter total marks");
+        float total_marks= scanner.nextFloat();
+        float percentage=(S1+S2+S3+S4+S5)/total_marks*100;
+        System.out.print("Your percentage is "+ percentage);
+
+
+
+    }
+}
